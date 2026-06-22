5b55883cbf467c2b39f6f0aaf224b44cac430624
shantilata <shantilatabiswal789@gmail.com>
2026-06-22 13:08:26 +0530

    js

---
+++ Month-1/Week-3/Day-1/Practice/script.js
@@ -0,0 +1,52 @@
+console.log("hello world !");
+
+a=30;
+console.log(a);
+
+A=50;
+console.log(A);
+
+st1="shantilata";
+console.log(st1);
+
+_=90;
+console.log(_)
+
+$=70;
+console.log($)
+
+full_name="hii";
+console.log(full_name);
+
+var a=40;
+
+console.log(a)
+
+{
+     var b=50;
+     console.log("block level access",b);
+
+}
+
+console.log("outside the block",b);
+
+function getV(){
+    var d=55;
+    console.log(d)
+}
+
+// console.log(d)
+getV()
+
+var num;
+console.log(num);
+
+// num=40;
+// console.log(num);
+
+// var num1=99;
+// console.log(num1);
+
+// var num;
+// console.log(num);
+
